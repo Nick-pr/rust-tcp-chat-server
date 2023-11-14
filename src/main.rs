cd8c3e615fe16bf1d98ea8bf1cfd0791ed36034b
Nick Raffaele <raffaele.nickp@gmail.com>
2023-11-14 18:03:55 -0500

    No longer echo user input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,48 @@
-use std::net::{SocketAddr, TcpListener};
-use std::sync::mpsc;
+use std::net::{SocketAddr, TcpListener, TcpStream};
 
-enum Message {}
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::broadcast;
 
-async fn handle_connection(_send_channel: mpsc::Sender<Message>, socket_addr: SocketAddr) {
+#[derive(Clone, Debug)]
+enum Event {
+    Message(Box<[u8]>, SocketAddr),
+}
+
+async fn handle_connection(
+    mut stream: tokio::net::TcpStream,
+    socket_addr: SocketAddr,
+    sender: broadcast::Sender<Event>,
+    mut receiver: broadcast::Receiver<Event>,
+) {
     tracing::info!("Connection on {} starting", socket_addr);
+
+    let (stream_reader, mut stream_writer) = stream.split();
+    let mut buf = String::new();
+
+    let mut stream_reader = BufReader::new(stream_reader);
+
+    loop {
+        tokio::select! {
+            _ =  stream_reader.read_line(&mut buf) => {
+                if buf.trim() == "/end" {
+                    break
+                }
+                sender.send(Event::Message(buf.as_bytes().into(), socket_addr)).unwrap();
+                buf.clear();
+            }
+            event = receiver.recv() => {
+                match event.unwrap() {
+                    Event::Message(m,author_addr) => {
+                        if author_addr == socket_addr {
+                            continue
+                        };
+                        stream_writer.write(&m).await.unwrap();
+                        stream_writer.flush().await.unwrap();
+                    }
+                }
+            }
+        }
+    }
     tracing::info!("Connection on {} closing", socket_addr);
 }
 
@@ -15,21 +53,25 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
 
-    let rt = tokio::runtime::Handle::current();
+    let (s, r) = std::sync::mpsc::channel::<(TcpStream, SocketAddr)>();
 
-    let (sender, recv) = mpsc::channel::<Message>();
-
+    // Thread to accept connections and push to channel
     std::thread::spawn(move || loop {
         match listener.accept() {
-            Ok((_, socket_addr)) => {
-                let sender = sender.clone();
-                rt.spawn(async move { handle_connection(sender, socket_addr).await });
-            }
+            Ok(conn) => s.send(conn).unwrap(),
             Err(err) => tracing::error!("{}", err),
         };
     });
 
-    for _message in recv {
-        // Todo
+    let tokio_rt = tokio::runtime::Handle::current();
+    let (sender, _) = broadcast::channel::<Event>(10);
+
+    // Infinite loop to spawn tokio tasks from the receiving end of the channel
+    for (stream, addr) in r {
+        stream.set_nonblocking(true).unwrap();
+        let stream = tokio::net::TcpStream::from_std(stream).unwrap();
+        let sender = sender.clone();
+        let receiver = sender.subscribe();
+        tokio_rt.spawn(async move { handle_connection(stream, addr, sender, receiver).await });
     }
 }
